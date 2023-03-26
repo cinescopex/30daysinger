edee2b4a1111eb6263e746ce44e94294793cba14
Cinescopex <cinescopex@gmail.com>
2023-03-26 19:43:22 +0800

    Apply UI changes

--- src/pages/Levels/Levels.jsx
+++ src/pages/Levels/Levels.jsx
@@ -2,6 +2,7 @@ import MainLayout from "../../components/MainLayout/MainLayout";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import data from "../Course/courses.json";
+import toCamelCase from "../../helpers/strings";
 
 const Levels = () => {
   const { level } = useParams();
@@ -9,18 +10,35 @@ const Levels = () => {
   return (
     <MainLayout>
       <div className="theme-content">
-        <h1 className="theme-title">{level}</h1>
+        <h1 className="theme-title">{toCamelCase(level)}</h1>
 
         <div className="theme-card-container">
           <div className="theme-card relative">
             {data.map((course) => {
               return (
-                <Link
-                  to={`/learn/${course.level}/${course.route}`}
-                  className="block hover:underline hover:text-sky-500 hover:cursor-pointer"
-                >
-                  {course.course}
-                </Link>
+                <div className="flex flex-col mb-8">
+                  <Link
+                    to={`/learn/${course.level}/${course.route}`}
+                    className="block hover:underline hover:text-sky-500 hover:cursor-pointer"
+                  >
+                    <img
+                      src={course.thumbnail}
+                      alt={`${course.course} by ${course.author}`}
+                    />
+                  </Link>
+                  <Link
+                    to={`/learn/${course.level}/${course.route}`}
+                    className="mt-2 text-lg font-bold hover:underline hover:text-sky-500 hover:cursor-pointer"
+                  >
+                    {course.course}
+                  </Link>
+                  <span className="flex flex-row gap-2">
+                    <strong>by</strong>
+                    <span>{course.author}</span>
+                  </span>
+                  <span className="mt-2">{course.description}</span>
+                  <hr className="theme-separator" />
+                </div>
               );
             })}
           </div>
